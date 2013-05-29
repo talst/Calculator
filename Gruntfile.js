8706db40c5a13e1a6f45f33e0437f036939db717
Aaron Cox <aaron.cox@greymass.com>
2013-05-29 18:44:47 -0500

    More file listening, should probably just change the filters.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
 		    files: [
 					'data/**/*.json',
 					'lib/**/*.js',
+					'lib/math/**/*.js',
 					'test/**/*.js',
 					'test/data/**/*.json',
 					'Gruntfile.js'

9cd1133601a6cf24003452535c92d600ed011bc7
Aaron Cox <aaron.cox@greymass.com>
2013-05-31 16:41:18 -0500

    Some coverage checking

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-	  notify_hooks: {
-	    options: {
-	      enabled: true,
-	      title: "d3up-calc"
-	    }
-	  },
+	grunt.initConfig({
+		notify_hooks: {
+			options: {
+				enabled: true,
+				title: "d3up-calc"
+			}
+		},
 		watch: {
-		  scripts: {
-		    files: [
+			scripts: {
+				files: [
 					'data/**/*.json',
 					'lib/**/*.js',
 					'lib/math/**/*.js',
@@ -16,60 +16,73 @@ module.exports = function(grunt) {
 					'test/data/**/*.json',
 					'Gruntfile.js'
 				],
-		    tasks: ['default']
-		  },
+				tasks: ['default']
+			},
 		},
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: false,
-        boss: true,
-        eqnull: true,
-        node: true,
-        strict: false,
-        globals: {
-          describe: false,
-          it: false,
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: false,
+				boss: true,
+				eqnull: true,
+				node: true,
+				strict: false,
+				globals: {
+					describe: false,
+					it: false,
 					setup: false,
 					teardown: false,
 					window: false,
-          suite: false,
-          test: false
-        }
-      },
-      all: {
-        src: ['grunt.js', 'lib/*.js', 'test/*.js']
-      }
-    },
-	  simplemocha: {
-	    options: {
-	      timeout: 3000,
-	      ui: 'tdd',
-	      reporter: 'spec'
-	    },
-
-	    all: { 
-				src: ['test/*_test.js'],
+					suite: false,
+					test: false
+				}
+			},
+			all: {
+				src: ['grunt.js', 'lib/*.js', 'test/*.js']
 			}
-	  }
-  });
+		},
+		simplemocha: {
+			all: { 
+				src: ['test/*_test.js'],
+				options: {
+					timeout: 3000,
+					ui: 'tdd',
+					reporter: 'spec'
+				}
+			}
+		},
+		coverage: {
+			options: {
+				thresholds: {
+					'statements': 99,
+					'branches': 99,
+					'lines': 99,
+					'functions': 99
+				},
+				dir: 'coverage',
+				root: 'lib'
+			}
+		}
+	});
 
 	if(!process.env.TRAVIS_NODE_VERSION) {
 		grunt.loadNpmTasks('grunt-notify');
-	  grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-istanbul-coverage');
 		grunt.task.run('notify_hooks');		
 	}
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-simple-mocha');
-	
-  grunt.registerTask('default', ['jshint', 'simplemocha']);
+
+	grunt.registerTask('default', ['jshint', 'simplemocha', 'coverage']);
+
 	// Travis CI task.
-  grunt.registerTask('travis', ['jshint', 'simplemocha']);
+	grunt.registerTask('travis', ['jshint', 'simplemocha', 'coverage']);
 };
